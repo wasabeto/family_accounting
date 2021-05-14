e62812b6bb542e05a41439eb01981547f498d002
Hector P <wasabeto@gmail.com>
2021-05-14 00:46:14 -0500

    Delete unnecessary Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:family_accounting/AppTheme.dart';
 import 'package:family_accounting/AppThemeNotifier.dart';
+import 'package:family_accounting/screens/auth/LoginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -24,70 +25,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
             debugShowCheckedModeBanner: false,
             theme: AppTheme.getThemeFromThemeMode(value.themeMode()),
-            home: MyHomePage(
-              title: 'Family accounting',
-            ));
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  ThemeData themeData;
-  CustomAppTheme customAppTheme;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    themeData = Theme.of(context);
-    return Consumer<AppThemeNotifier>(
-      builder: (BuildContext context, AppThemeNotifier value, Widget child) {
-        customAppTheme = AppTheme.getCustomAppTheme(value.themeMode());
-        return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.getThemeFromThemeMode(value.themeMode()),
-            home: Scaffold(
-              backgroundColor: themeData.colorScheme.background,
-              appBar: AppBar(
-                backgroundColor: customAppTheme.bgLayer2,
-                title: Text(widget.title, style: AppTheme.getTextStyle(themeData.textTheme.headline6, fontWeight: 600)),
-              ),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'You have pushed the button this many times:',
-                    ),
-                    Text(
-                      '$_counter',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ],
-                ),
-              ),
-              floatingActionButton: FloatingActionButton(
-                backgroundColor: themeData.colorScheme.primary,
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-            ));
+            home: LoginScreen());
       },
     );
   }
